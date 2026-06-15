b7b381d286156a8e043dbaf620cbc159d347a40e
Cameron Aziz <cameronaziz@me.com>
2026-06-15 15:38:36 -0700

    implement

--- provision/pipeline/migrations/0001_initial.down.sql
+++
@@ -1,6 +0,0 @@
-DROP INDEX IF EXISTS idx_jobs_updated;
-DROP INDEX IF EXISTS idx_jobs_company;
-DROP INDEX IF EXISTS idx_jobs_owner;
-DROP INDEX IF EXISTS idx_jobs_status;
-DROP TABLE IF EXISTS jobs;
-DROP TABLE IF EXISTS companies;
--- provision/pipeline/migrations/0004_schema_operations.down.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS schema_operation_logs;
-DROP TABLE IF EXISTS schema_operations;
-
-CREATE TABLE IF NOT EXISTS schema_migrations (
-  version     INTEGER PRIMARY KEY,
-  applied_at  TEXT NOT NULL DEFAULT (datetime('now'))
-);
